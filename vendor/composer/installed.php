e143e57dbba60800d13fa7890a1a692813ccb805
vini <viniciusdnb@hotmail.com>
2022-03-12 17:41:02 -0300

    alterando o gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1e98b36419d67ee57c99d10eebdb7b2c3dbb4f1d',
+        'name' => 'vini/portdigital',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vini/portdigital' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1e98b36419d67ee57c99d10eebdb7b2c3dbb4f1d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
